60106478041bac092887703c7e39f4f66e8ab7fa
monicaep <mep823@gmail.com>
2019-09-09 13:29:54 -0700

    issue 4: first point completed

---
+++ spec/integration/topics_spec.js
@@ -0,0 +1,133 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/topics/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+
+describe("routes : topics", () => {
+
+  beforeEach((done) => {
+    this.topic;
+    sequelize.sync({force: true}).then((res) => { //clear database
+      Topic.create({
+        title: "Pets"
+      })
+      .then((topic) => {
+        this.topic = topic;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+  });
+
+  describe("GET /topics", () => {
+    it("should return a status code 200 and all topics", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(err).toBeNull();
+        expect(body).toContain("Topics");
+        expect(body).toContain("Pets");
+        done();
+      });
+    });
+  });
+
+  describe("GET /topics/new", () => {
+    it("should render a new topic form", (done) => {
+      request.get(`${base}new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Topic");
+        done();
+      });
+    });
+  });
+
+  describe("POST /topics/create", () => {
+    const options = {
+      url: `${base}create`,
+      form: {
+        title: "Money"
+      }
+    };
+
+    it("should create a new topic and redirect", (done) => {
+      request.post(options, (err, res, body) => {
+        Topic.findOne({where: {title: "Money"}})
+        .then((topic) => {
+          expect(res.statusCode).toBe(303);
+          expect(topic.title).toBe("Money");
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+  });
+
+  describe("GET /topics/:id", () => {
+    it("should render a view with the selected topic", (done) => {
+      request.get(`${base}${this.topic.id}`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Pets");
+        done();
+      });
+    });
+  });
+
+  describe("POST /topics/:id/destroy", () => {
+    it("should delete the topic with the associated ID", (done) => {
+      Topic.findAll()
+      .then((topics) => {
+        const topicCountBeforeDelete = topics.length;
+        expect(topicCountBeforeDelete).toBe(1);
+
+        request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+          Topic.findAll()
+          .then((topics) => {
+            expect(err).toBeNull();
+            expect(topics.length).toBe(topicCountBeforeDelete - 1);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe("GET /topics/:id/edit", () => {
+    it("should render a view with an edit topic form", (done) => {
+      request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Topic");
+        expect(body).toContain("Pets");
+        done();
+      });
+    });
+  });
+
+  describe("POST /topics/:id/update", () => {
+    it("should update the topic with the given values", (done) => {
+      const options ={
+        url: `${base}${this.topic.id}/update`,
+        form: {
+          title: "Furry Creatures"
+        }
+      };
+      request.post(options, (err, res, body) => {
+        expect(err).toBeNull();
+
+        Topic.findOne({
+          where: {id: this.topic.id}
+        })
+        .then((topic) => {
+          expect(topic.title).toBe("Furry Creatures");
+          done();
+        });
+      });
+    });
+  });
+});
---
+++ src/db/queries.topics.js
@@ -0,0 +1,67 @@
+const Topic = require("./models").Topic;
+
+module.exports = {
+  getAllTopics(callback) {
+    return Topic.findAll()
+
+    .then((topics) => {
+      callback(null, topics);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  getTopic(id, callback) {
+    return Topic.findByPk(id)
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  addTopic(newTopic, callback) {
+    return Topic.create({
+      title: newTopic.title
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  updateTopic(id, updatedTopic, callback) {
+    return Topic.findByPk(id)
+    .then((topic) => {
+      if(!topic) {
+        return callback("Topic not found");
+      }
+
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+    });
+  },
+
+  deleteTopic(id, callback) {
+    return Topic.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+}
